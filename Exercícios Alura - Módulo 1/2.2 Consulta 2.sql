607b4198814c3df5e69a7130a677f6a5a708d641
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-26 21:49:36 -0300

    Create 2.2 Consulta 2.sql

---
+++ Exercícios Alura - Módulo 1/2.2 Consulta 2.sql
@@ -0,0 +1,38 @@
+-- Consulta 2: Retornar o nome e a nota do aluno que possui a melhor nota na disciplina de Matemática
+
+-- Ordenando as notas dos alunos da disciplina matemática pelo maior valor e 
+-- destacando o primeiro valor (o maior) com a cláusula LIMIT 1
+select
+	alunos.nome_aluno, notas.valor_nota, disciplinas.nome_disciplina
+from
+	notas join disciplinas on notas.id_disciplina_fk = disciplinas.id_disciplina
+    join alunos on notas.id_aluno_fk = alunos.id_aluno
+where
+	disciplinas.nome_disciplina = 'Matemática'
+order by
+	notas.valor_nota desc
+LIMIT 1;
+
+
+-- Versão alternativa mais complicada usando a função max() dentro de uma subconsulta no where
+select
+	alunos.nome_aluno, notas.valor_nota, disciplinas.nome_disciplina
+from
+	notas join disciplinas on notas.id_disciplina_fk = disciplinas.id_disciplina
+    join alunos on notas.id_aluno_fk = alunos.id_aluno
+where
+	disciplinas.nome_disciplina = 'Matemática' 
+    and 
+    notas.valor_nota = (
+		select 
+			max(notas.valor_nota)
+		from
+			notas join disciplinas on notas.id_disciplina_fk = disciplinas.id_disciplina
+		where
+			disciplinas.nome_disciplina = 'Matemática'
+    );
+
+/*
+Resultado:
+Carolina Oliveira - 9.40 - Matemática
+*/
