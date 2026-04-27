9f25dcf0e45aa30bc41b109c2b98e55e7cb98718
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-26 22:23:43 -0300

    Update 2.5 Consulta 5.sql

--- Exercícios Alura - Módulo 1/2.5 Consulta 5.sql
+++ Exercícios Alura - Módulo 1/2.5 Consulta 5.sql
@@ -1,14 +1,15 @@
 -- Consulta 5: Criar uma view que apresenta o nome, a disciplina e a nota dos alunos
 
--- cria a view info_alunos com 3 campos: nome do aluno, nome da disciplina e as notas
+-- Cria a view info_alunos com 3 campos: nome do aluno, nome da disciplina e as notas,
+-- ordenando da maior nota para a menor
 create view info_alunos as
 select
 	alunos.nome_aluno, disciplinas.nome_disciplina, notas.valor_nota
 from
-	alunos join turma_alunos on alunos.id_aluno = turma_alunos.id_aluno_fk
-    join turma_disciplinas on turma_disciplinas.id_turma_fk = turma_alunos.id_turma_fk
-    join disciplinas on disciplinas.id_disciplina = turma_disciplinas.id_disciplina_fk
-    join notas on notas.id_disciplina_fk = disciplinas.id_disciplina;
+	alunos join notas on alunos.id_aluno = notas.id_aluno_fk
+    join disciplinas on disciplinas.id_disciplina = notas.id_disciplina_fk
+order by
+	alunos.nome_aluno, notas.valor_nota desc;
     
--- Mostra os alunos, a disciplina e as notas
+-- Mostra a view criada com os alunos, a disciplina e as notas
 select * from info_alunos;
